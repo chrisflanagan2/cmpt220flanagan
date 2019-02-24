7cbe9e12027f99e34b7fa5c05f734b29707bc7bd
Juan Arias <juan.arias@marist.edu>
2019-02-24 10:45:43 -0500

    Feedback

--- labs/3/lab3_4.java
+++ labs/3/lab3_4.java
@@ -1,37 +1,29 @@
 package lab3;
-
+// JA: This needs to be formatted
 public class lab3_4 {
 	
-public static void main(String[] args) {
+	public static void main(String[] args) {
+		int n;
+		int i=0;
 
-int n;
-int i=0;
-
-for(n=14999;n>0;n--)
-
-{ 
-
-for(int j=1;i<=n;j++)
-
-{
-
-if(n%j==0)
-
-{
-
-j++;
-
-}}
-
-
-
-if(i==2)
-
-break;
-
+		for(n=14999;n>0;n--)
+		{ 
+			if (isPrime(n))
+				break;
+		}
+		System.out.println("Largest n that is prime and less than 15000 is "+n);
+	}
+	
+	public static boolean isPrime(int n) {
+		for(int j=2;j<n;j++)
+		{
+			if(n%j==0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
 }
 
-System.out.println("Largest n that is prime and less than 15000 is "+n);
-
-}}
 
